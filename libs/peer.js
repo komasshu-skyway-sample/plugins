f640c8b4e8a3bf22c62e91cafc75768478b2b8e6
kensakukomatsu <kensaku.komatsu@gmail.com>
2015-05-26 01:43:42 +0900

    for issue #14 fix bug for create & set offer while it is callee

--- libs/peer.js
+++ libs/peer.js
@@ -599,6 +599,9 @@ Negotiator.cleanup = function(connection) {
 
 Negotiator._makeOffer = function(connection) {
   var pc = connection.pc;
+
+  if(!!pc.remoteDescription && !!pc.remoteDescription.type) return;
+
   pc.createOffer(function(offer) {
     util.log('Created offer.');
 
